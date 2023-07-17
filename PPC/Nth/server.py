d74e3165b4556f31bbe0053ae7f6693a3400e956
Curious-Lucifer <curious.lucifer.domini@gmail.com>
2023-07-17 14:48:05 +0800

    Fix Nth

--- PPC/Nth/server.py
+++ PPC/Nth/server.py
@@ -15,6 +15,7 @@ def main():
     num_list = [random.randint(100, 99999) for i in range(300)]
     nth = random.randint(0, 299)
     print('numbers : ' + ' '.join(str(num) for num in num_list))
+    print(f'nth : {nth}')
 
     input_num = int(input('answer > '))
 
